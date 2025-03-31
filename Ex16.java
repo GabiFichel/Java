a57daa7717c0c1d56ed66babb6e5b1edd63b0052
GabiFichel <mariagabrielasantos1a@gmail.com>
2025-03-31 14:00:57 -0300

    Nome 10 vezes

---
+++ Ex16.java
@@ -0,0 +1,14 @@
+// 16) Leia o nome do usuário e escreva o nome dele na tela 10 vezes.
+import java.util.Scanner;
+
+public class Ex16 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+       System.out.print("Digite seu nome: ");
+        String nome = ler.nextLine();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nome);
+        }
+    }
+}
