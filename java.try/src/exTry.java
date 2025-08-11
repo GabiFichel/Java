8942cb36572c3f4dcbb4363375bf96d82d3e7b79
Gabriela Fichel <mariagabrielasantos1a@gmail.com>
2025-08-11 13:26:12 -0300

    Add files via upload

---
+++ java.try/src/exTry.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class exTry {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int a, b, c;
+        a = 10;
+        System.out.println("Digite um número:");
+        b = ler.nextInt();
+        try{
+            c = a/ b;
+            System.out.println("Valor de C: " + c);
+        } catch (ArithmeticException erro) {
+            System.out.println("Não existe divisão por zero");
+        }
+    }
+}
